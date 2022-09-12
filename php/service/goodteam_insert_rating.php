a5bdaed61b74242467243fd96acdb5984ba8de7e
dookda <sakda.homhuan@gmail.com>
2022-09-12 08:52:35 +0700

    update

--- php/service/goodteam_insert_rating.php
+++ php/service/goodteam_insert_rating.php
@@ -6,13 +6,18 @@
 			$sql = "INSERT INTO team_rating(cid)VALUES('$cid')";
 			mysqli_query($objCon,$sql);
 			$query = "";
+			$sumRate = 0;
 			foreach ($_POST as $key => $value) {
 				if(!empty($value) && $key != 'cid'){
 					$sql2 = "UPDATE team_rating SET $key='$value' WHERE cid='$cid'";
-					// echo $sql2;
+					$sumRate += $value;
 					$query = mysqli_query($objCon,$sql2);
 				}
 			}
+
+			$sql2 = "UPDATE team_rating SET sumrating=$sumRate WHERE cid='$cid'";
+			mysqli_query($objCon,$sql);
+			
 			if($query) {
 				header("location:./../_report_team.html");
 			}
