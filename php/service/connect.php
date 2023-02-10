3e4fd4f627aa7990c99349e24bda08d3ef8ac0e8
sakda homhuan <sakda.homhuan@gmail.com>
2023-02-10 16:46:16 +0700

    odle pass

--- php/service/connect.php
+++ php/service/connect.php
@@ -1,17 +1,18 @@
 <?php
-$db_config = array(
-    "host" => "db",  // กำหนด host
-    "user" => "admin", // กำหนดชื่อ user      root
-    "pass" => "AAaa2023", //                    AAaa@!2019
+	$db_config=array(
+    "host"=>"db",  // กำหนด host
+    "user"=>"sakda", // กำหนดชื่อ user      root
+    "pass"=>"1234",//                    AAaa@!2019
     // "pass"=>"qazwsxedcr112233",   // กำหนดรหัสผ่าน
-    "dbname" => "jomnam2",  // กำหนดชื่อฐานข้อมูล
-    "charset" => "utf8"  // กำหนด charset
+    "dbname"=>"jomnam2",  // กำหนดชื่อฐานข้อมูล
+    "charset"=>"utf8"  // กำหนด charset
 );
 $objCon = @new mysqli($db_config["host"], $db_config["user"], $db_config["pass"], $db_config["dbname"]);
 
-if (mysqli_connect_error()) {
-    die('Connect Error (' . mysqli_connect_errno() . ') ' . mysqli_connect_error());
+if(mysqli_connect_error()) {
+    die('Connect Error (' . mysqli_connect_errno() . ') '. mysqli_connect_error());
     exit;
 }
 
 $objCon->set_charset("utf8");
+?>
