7cbb7cf147852c50e6de98a9b082be75c08580ae
sakda homhuan <sakda.homhuan@gmail.com>
2023-02-10 16:14:14 +0700

    update db

--- php/service/connect.php
+++ php/service/connect.php
@@ -1,19 +1,17 @@
 <?php
-	$db_config=array(
-    "host"=>"db",  // กำหนด host
-    "user"=>"sakda", // กำหนดชื่อ user      root
-    "pass"=>"1234",//                    AAaa@!2019
+$db_config = array(
+    "host" => "db",  // กำหนด host
+    "user" => "admin", // กำหนดชื่อ user      root
+    "pass" => "12345", //                    AAaa@!2019
     // "pass"=>"qazwsxedcr112233",   // กำหนดรหัสผ่าน
-    "dbname"=>"jomnam2",  // กำหนดชื่อฐานข้อมูล
-    "charset"=>"utf8"  // กำหนด charset
+    "dbname" => "jomnam2",  // กำหนดชื่อฐานข้อมูล
+    "charset" => "utf8"  // กำหนด charset
 );
 $objCon = @new mysqli($db_config["host"], $db_config["user"], $db_config["pass"], $db_config["dbname"]);
 
-if(mysqli_connect_error()) {
-    die('Connect Error (' . mysqli_connect_errno() . ') '. mysqli_connect_error());
+if (mysqli_connect_error()) {
+    die('Connect Error (' . mysqli_connect_errno() . ') ' . mysqli_connect_error());
     exit;
 }
-
-// test
 
 $objCon->set_charset("utf8");
